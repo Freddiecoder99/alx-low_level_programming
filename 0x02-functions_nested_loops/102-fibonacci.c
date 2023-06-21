343381881b4c579de2eeba1c23ea94ca1aa009ba
Freddiecoder99 <rickyfredo77@gmail.com>
2023-06-21 14:54:43 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
+/**
+ * Calculates and prints the Fibonacci sequence up to the 50th term.
+ */
 int main(void)
 {
     int count, num1 = 1, num2 = 2, next;
 
     printf("%d, %d", num1, num2);
 
-    for (count = 3; count <= 50; count++)
-    {
+    for (count = 3; count <= 50; count++) {
         next = num1 + num2;
         printf(", %d", next);
         num1 = num2;
