0c954fd99e1af024bcde4f9b1991c7e2246395d4
Freddiecoder99 <rickyfredo77@gmail.com>
2023-06-21 10:51:51 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,21 +7,21 @@
  */
 int main(void)
 {
-    int num1 = 1, num2 = 2, next, sum = 2;
+	int num1 = 1, num2 = 2, next, sum = 2;
 
-    while (num2 <= 4000000)
-    {
-        next = num1 + num2;
+	while (num2 <= 4000000)
+	{
+		next = num1 + num2;
 
-        if (next % 2 == 0)
-            sum += next;
+		if (next % 2 == 0)
+			sum += next;
 
-        num1 = num2;
-        num2 = next;
-    }
+		num1 = num2;
+		num2 = next;
+	}
 
-    printf("%d\n", sum);
+	printf("%d\n", sum);
 
-    return 0;
+	return 0;
 }
 
