448ca8ff6c4b32142cb05ea728565240c98a8822
Freddiecoder99 <rickyfredo77@gmail.com>
2023-06-21 10:48:19 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int num1 = 1, num2 = 2, next, sum = 2;
+
+    while (num2 <= 4000000)
+    {
+        next = num1 + num2;
+
+        if (next % 2 == 0)
+            sum += next;
+
+        num1 = num2;
+        num2 = next;
+    }
+
+    printf("%d\n", sum);
+
+    return 0;
+}
+
