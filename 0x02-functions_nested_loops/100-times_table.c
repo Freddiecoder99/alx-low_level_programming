24d74687747ef33261702a5f6f84441ec1622bc6
Freddiecoder99 <rickyfredo77@gmail.com>
2023-06-21 16:56:13 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -30,7 +30,7 @@ void print_times_table(int n)
                 _putchar(' ');
             }
             else if (result < 100)
-            {
+           {
                 _putchar(' ');
                 _putchar(' ');
             }
@@ -46,4 +46,3 @@ void print_times_table(int n)
         _putchar('\n');
     }
 }
-
