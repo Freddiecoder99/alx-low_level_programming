a60e31c825e3a6e7b626aa2b5dac7f12bc91dceb
Freddiecoder99 <rickyfredo77@gmail.com>
2023-07-07 14:17:08 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,9 +10,9 @@
  */
 int main(int argc, char *argv[])
 {
-    (void)argv; /* Ignore argv */
-    printf("%d\n", argc - 1);
+	(void)argv; /* Ignore argv */
+	printf("%d\n", argc - 1);
 
-    return (0);
+	return (0);
 }
 
