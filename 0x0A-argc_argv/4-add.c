1a157abd6ab903cc1e4ffbb1cccbd58d2841988a
Freddiecoder99 <rickyfredo77@gmail.com>
2023-07-07 14:01:17 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,17 +11,17 @@
  */
 int check_num(char *str)
 {
-    unsigned int count = 0;
+	unsigned int count = 0;
 
-    while (count < strlen(str))
-    {
-        if (!isdigit(str[count]))
-        {
-            return (0);
-        }
-        count++;
-    }
-    return (1);
+	while (count < strlen(str))
+	{
+		if (!isdigit(str[count]))
+		{
+			return (0);
+		}
+		count++;
+	}
+	return (1);
 }
 
 /**
@@ -33,26 +33,26 @@ int check_num(char *str)
  */
 int main(int argc, char *argv[])
 {
-    int count, str_to_int, sum = 0;
+	int count, str_to_int, sum = 0;
 
-    count = 1;
-    while (count < argc)
-    {
-        if (check_num(argv[count]))
-        {
-            str_to_int = atoi(argv[count]);
-            sum += str_to_int;
-        }
-        else
-        {
-            printf("Error\n");
-            return (1);
-        }
-        count++;
-    }
+	count = 1;
+	while (count < argc)
+	{
+		if (check_num(argv[count]))
+		{
+			str_to_int = atoi(argv[count]);
+			sum += str_to_int;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		count++;
+	}
 
-    printf("%d\n", sum);
+	printf("%d\n", sum);
 
-    return (0);
+	return (0);
 }
 
