467ea2643b3d0b5e9c804287c4f210ff05465290
Freddiecoder99 <rickyfredo77@gmail.com>
2023-07-10 11:26:34 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -8,10 +9,7 @@
  */
 int count_word(char *s)
 {
-	int flag, c, w;
-
-	flag = 0;
-	w = 0;
+	int flag = 0, c, w = 0;
 
 	for (c = 0; s[c] != '\0'; c++)
 	{
@@ -28,23 +26,45 @@ int count_word(char *s)
 }
 
 /**
+ * allocate_word - allocate memory and copy a word from the string
+ * @str: string containing words
+ * @start: starting index of the word
+ * @end: ending index of the word
+ * Return: pointer to the allocated word
+ */
+char *allocate_word(char *str, int start, int end)
+{
+	int i, length = end - start;
+	char *word = malloc(sizeof(char) * (length + 1));
+
+	if (word == NULL)
+		return (NULL);
+
+	for (i = 0; i < length; i++)
+		word[i] = str[start + i];
+
+	word[length] = '\0';
+	return (word);
+}
+
+/**
  * strtow - splits a string into words
  * @str: string to split
  * Return: pointer to an array of strings (Success) or NULL (Error)
  */
 char **strtow(char *str)
 {
-	char **matrix, *tmp;
+	char **matrix, *word;
 	int i, k = 0, len = 0, words, c = 0, start, end;
 
-	while (*(str + len))
+	while (str[len])
 		len++;
 
 	words = count_word(str);
 	if (words == 0)
 		return (NULL);
 
-	matrix = (char **)malloc(sizeof(char *) * (words + 1));
+	matrix = malloc(sizeof(char *) * (words + 1));
 	if (matrix == NULL)
 		return (NULL);
 
@@ -55,14 +75,15 @@ char **strtow(char *str)
 			if (c)
 			{
 				end = i;
-				tmp = (char *)malloc(sizeof(char) * (c + 1));
-				if (tmp == NULL)
+				word = allocate_word(str, start, end);
+				if (word == NULL)
+				{
+					for (i = 0; i < k; i++)
+						free(matrix[i]);
+					free(matrix);
 					return (NULL);
-				while (start < end)
-					tmp[start - k] = str[start], start++;
-				tmp[start - k] = '\0';
-				matrix[k] = tmp;
-				k++;
+				}
+				matrix[k++] = word;
 				c = 0;
 			}
 		}
@@ -71,7 +92,6 @@ char **strtow(char *str)
 	}
 
 	matrix[k] = NULL;
-
 	return (matrix);
 }
 
